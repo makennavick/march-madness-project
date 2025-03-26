e412c18025f5ca2dbfac13e7e9c2fc382655ecf4
duvoemoua <duvoemoua@gmail.com>
2025-03-25 19:43:59 -0500

    swapping images

--- Data_cleaning/NCAA_Schema.sql
+++ Data_cleaning/NCAA_Schema.sql
@@ -1,37 +1,39 @@
-DROP TABLE IF EXISTS NCAA_Tournament_Stats;
+DROP TABLE IF EXISTS barttorvik_all_years;
 
-CREATE TABLE NCAA_Tournament_Stats (
+CREATE TABLE barttorvik_all_years (
     Year INT,
-    Rk INT,
+    Seed INT,
     School VARCHAR(100),
-    Seed_Rank VARCHAR(10),
-    Round_Finished VARCHAR(50),
-    Conf VARCHAR(10),
+    Round_Finished FLOAT,
+    Conf VARCHAR(50),
     G INT,
     Rec VARCHAR(10),
     AdjOE FLOAT,
     AdjDE FLOAT,
+    AdjNR FLOAT,
     Barthag FLOAT,
-    EFG_ FLOAT,       
-    EFGD_ FLOAT,      
+    EFG FLOAT,
+    EFGD FLOAT,
     TOR FLOAT,
     TORD FLOAT,
     ORB FLOAT,
     DRB FLOAT,
     FTR FLOAT,
     FTRD FLOAT,
-    TwoP_ FLOAT,      
-    TwoPD_ FLOAT,     
-    ThreeP_ FLOAT,    
-    ThreePD_ FLOAT,   
-    ThreePR FLOAT,    
-    ThreePRD FLOAT,   
+    TwoP FLOAT,      -- Represents 2P%
+    TwoPD FLOAT,     -- Represents 2P%D
+    ThreeP FLOAT,    -- Represents 3P%
+    ThreePD FLOAT,   -- Represents 3P%D
+    ThreePR FLOAT,   -- Represents 3PR
+    ThreePRD FLOAT,  -- Represents 3PRD
     Adj_T FLOAT,
-    WAB FLOAT,
-    PRIMARY KEY (Year, Rk)
+    WAB FLOAT
 );
 
+SELECT *
+FROM barttorvik_all_years
+WHERE Year = 2023;
 
-SELECT * 
-FROM NCAA_Tournament_Stats 
-WHERE Year = 2009;
+SELECT *
+FROM barttorvik_all_years
+WHERE Seed = 1;
